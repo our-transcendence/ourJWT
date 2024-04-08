7bb17a9bbe458d2006fcf58d0ecae634cefa29d1
gd-harco <gd-harco@student.42lyon.fr>
2024-04-08 15:37:03 +0200

    Added __str__ overload in exception and added BadSubject exception

--- src/OUR_exception.py
+++ src/OUR_exception.py
@@ -1,8 +1,17 @@
 class RefusedToken(Exception):
-    pass
+    def __str__(self):
+        return "Cannot decode Token"
+
 
 class ExpiredToken(Exception):
-    pass
+    def __str__(self):
+        return "token has expired"
+
 
 class NoKey(Exception):
-    pass
+    def __str__(self):
+        return "Couldn't locate public/private key"
+
+class BadSubject(Exception):
+    def __str__(self):
+        return "Bad subject in token"
