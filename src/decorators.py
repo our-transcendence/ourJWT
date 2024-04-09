e6fef2ae162e1fdc12bf1f8f4553447fa89eb5b7
gd-harco <gd-harco@student.42lyon.fr>
2024-04-09 16:32:29 +0200

    first draft at "auth_required" decorator

---
+++ src/decorators.py
@@ -0,0 +1,21 @@
+import jwt
+from django.http import HttpRequest, response
+
+import OUR_exception, OUR_class
+
+def auth_required(function):
+    def wrapper(request: HttpRequest, *args, **kwargs):
+        auth = request.COOKIES.get("auth_token", None)
+        if auth is None:
+            return response.HttpResponse(status=412, reason="No auth token detected")
+        try:
+            auth_decoded = OUR_class.Decoder.decode(auth)
+        except OUR_exception.NoKey:
+            print("NO PUBLIC KEY BIG ERROR") #TODO check with beroux to standardize thoose type of error
+        except (OUR_exception.BadSubject, OUR_exception.RefusedToken):
+            return response.HttpResponse(status=412, reason="Bad auth token")
+        except OUR_exception.ExpiredToken:
+            return response.HttpResponse(status=412, reason="Expired auth token") #TODO: generate new auth token directly from here
+        return function(request)
+
+    return wrapper
